44643950e168e09217382c677a3b142438f53142
irfanalam0123 <irfanalam0123bth@gmail.com>
2024-12-30 01:48:42 +0530

    mern-app

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/image333.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills-section--heading">About Me</h1>
+          <p className="hero--section-description">
+            With a passion for creating dynamic and user-friendly web
+            applications, I bring a combination of technical expertise and
+            creative problem-solving skills to deliver seamless experiences and
+            robust solutions.
+          </p>
+          <p className="hero--section-description">
+            Whether it's building intuitive interfaces or designing scalable
+            server-side architecture, I aim to make every project efficient,?
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,25 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Irfan jahan</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color">Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section-description ">
+            With a passion for creating dynamic and user-friendly web
+            applications,
+            <br /> I specialize in both front-end and back-end development
+          </p>
+        </div>
+        <button className="btn btn-primary">Get In Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/homeimage.png" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
